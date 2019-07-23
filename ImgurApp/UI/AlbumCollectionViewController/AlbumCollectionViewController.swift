5979dd7c6e59d7de8818a196d1ab9393ee822ec2
Jake Elrod <stephen.jacob.elrod@gmail.com>
2019-07-23 00:31:24 -0500

    added some error handling

--- ImgurApp/UI/AlbumCollectionViewController/AlbumCollectionViewController.swift
+++ ImgurApp/UI/AlbumCollectionViewController/AlbumCollectionViewController.swift
@@ -26,7 +26,8 @@ class AlbumCollectionViewController: UICollectionViewController {
     }
     
     func reload() {
-        if let username = self.username {
+        if let username = self.username,
+            !username.isEmpty {
             self.title = "\(username)'s Albums"
             self.albums = [Album]()
             self.collectionView.reloadData()
@@ -37,7 +38,11 @@ class AlbumCollectionViewController: UICollectionViewController {
                     self?.collectionView.reloadData()
                 }
             }) { (error) in
-                print(error)
+                let alert = UIAlertController(title: "Whoops!", message: "Looks like an problem occurred. Please make sure you're connected to the internet and '\(username)' is a valid user.", preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "Ok", style: .default))
+                DispatchQueue.main.async { [weak self] in
+                    self?.present(alert, animated: true)
+                }
             }
         } else {
             self.title = nil
@@ -59,18 +64,24 @@ class AlbumCollectionViewController: UICollectionViewController {
     }
     
     override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        self.selectedIndex = indexPath.item
-        self.performSegue(withIdentifier: "albumImages", sender: self)
+        if self.albums.count > 0 {
+            self.selectedIndex = indexPath.item
+            self.performSegue(withIdentifier: "albumImages", sender: self)
+        }
     }
     
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return albums.count
+        return self.albums.count == 0 ? 1 : self.albums.count
     }
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "albumCell", for: indexPath) as! AlbumCollectionViewCell
-        cell.album = self.albums[indexPath.item]
-        return cell
+        if self.albums.count > 0 {
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "albumCell", for: indexPath) as! AlbumCollectionViewCell
+            cell.album = self.albums[indexPath.item]
+            return cell
+        } else {
+            return collectionView.dequeueReusableCell(withReuseIdentifier: "noAlbums", for: indexPath)
+        }
     }
 }
 
@@ -78,10 +89,14 @@ extension AlbumCollectionViewController: UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView,
                         layout collectionViewLayout: UICollectionViewLayout,
                         sizeForItemAt indexPath: IndexPath) -> CGSize {
-        //two albums per row with 8 margin on sides and in middle
-        let width = (UIScreen.main.bounds.width - 24) * 0.5
-        let height = width * 1.3
-        return CGSize(width: width, height: height)
+        if self.albums.count > 0 {
+            //two albums per row with 8 margin on sides and in middle
+            let width = (UIScreen.main.bounds.width - 24) * 0.5
+            let height = width * 1.3
+            return CGSize(width: width, height: height)
+        } else {
+            return CGSize(width: collectionView.bounds.size.width, height: 300)
+        }
     }
 }
 
